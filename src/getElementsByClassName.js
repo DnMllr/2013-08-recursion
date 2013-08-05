29737348fec5fb8ddab8f7a27c69f232592e802b
DnMllr <yajnavalkya@cox.net>
2013-08-05 00:37:36 -0700

    getElementsByClassName works

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,21 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var arr = [];
+  var node = document.body;
+  var recurse = function(hello){
+  	for (var i = 0 ; i < hello.childNodes.length ; i++) {
+  		if (hello.childNodes[i].nodeName !== "#text") {
+	  		var hello2 = hello.childNodes[i];
+	  		if (hello2.classList.contains(className)) {
+	  			arr.push(hello2);
+	  		}
+	  		if (hello2.childNodes.length > 0) {
+	  			recurse(hello2);
+	  		}
+  		}
+  	}
+  }
+  recurse(node);
+  return arr;
 };
